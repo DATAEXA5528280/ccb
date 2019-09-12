13f9491a79fd7e02d0026398871b6ab0966a24e5
朱晓晶 <zhuxiaojing@dataexa.com>
2019-09-12 14:01:45 +0800

    “提交文件”

---
+++ src/site/HelloWorld.java
@@ -0,0 +1,13 @@
+ public class HelloWorld{
+
+               //程序的主方法，是程序的入口
+
+               public static void main(String args[]){
+
+                   //要执行的代码
+
+                   System.out.println("HelloWorld");
+
+         }
+
+    }
